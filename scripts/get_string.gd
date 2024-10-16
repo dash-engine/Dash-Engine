4517aac1ca4bc05ff0a1d044461372b66d1ae202
MikeDev_ <166942889+MikeDeev@users.noreply.github.com>
2024-10-16 21:05:27 +0200

    file manager thing

--- scripts/get_string.gd
+++ scripts/get_string.gd
@@ -1,18 +1,16 @@
 extends Control
 
-@export var question : String
-
+@export var question: String
 @export var response = ""
-
-@export var textEdit : TextEdit
+@export var textEdit: TextEdit
 
 signal accepted
 
-# FIXME: For some reason, you cant type on the textbox
-
-func _process(delta: float) -> void:
+func _ready() -> void:
 	textEdit.text = question
+	textEdit.grab_focus()
 
 func _on_accept_pressed() -> void:
+	print("Accept")
 	response = textEdit.text
 	accepted.emit()
