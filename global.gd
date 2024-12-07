60b695b9a392cd28891730f26428005833525d46
MikeDev_ <166942889+MikeDeev@users.noreply.github.com>
2024-12-07 16:03:38 +0100

    change splash

--- global.gd
+++ global.gd
@@ -50,12 +50,12 @@ func _ready() -> void:
 	saveScene(mainSceneUID,"main",sceneGroup+1)
 	
 	var splashObjUID = UID.generate()
-	saveObject(splashObjUID,"Splash", Vector2(400,200), 1, 0, splashSceneUID)
+	saveObject(splashObjUID,"Splash", Vector2(384, 144), 1, 0, splashSceneUID)
 	saveScript(splashObjUID, """set_transparency(0)
 
 Game.create_text("Made with dash engine",position.x,position.y,0g,1)
 
-wait(6)
+wait(3)
 
 Game.change_scene("main")""", 2)
 	
